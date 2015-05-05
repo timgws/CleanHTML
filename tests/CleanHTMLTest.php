f3efdc868a831c815094f23ffa047340e12364ed
Tim Groeneveld <tim@timg.ws>
2015-05-05 14:31:30 +1000

    Test init'ing the library!

---
+++ tests/CleanHTMLTest.php
@@ -0,0 +1,9 @@
+<?php
+use \timgws\CleanHTML\CleanHTML;
+
+class CleanHTMLTest extends PHPUnit_Framework_TestCase {
+    public function testInitCleanHTML()
+    {
+        $cleanhtml = new CleanHTML();
+    }
+}
