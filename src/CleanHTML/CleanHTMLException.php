c96763fc0a5726a2e7a8015f6212306351944cb3
Tim Groeneveld <tim@timg.ws>
2015-05-05 14:31:56 +1000

    Add the CleanHTMLException.

---
+++ src/CleanHTML/CleanHTMLException.php
@@ -0,0 +1,6 @@
+<?php
+namespace timgws\CleanHTML;
+
+class CleanHTMLException extends \Exception {
+
+}
---
+++ tests/HTMLPurifierInstanceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class HTMLPurifierInstanceTest extends PHPUnit_Framework_TestCase {
+    private $config;
+    private $purifier;
+
+    public function testCanLoadHTMLPurifier ()
+    {
+        $this->config = HTMLPurifier_Config::createDefault();
+        $this->config->set('Core.EscapeNonASCIICharacters', false);
+        $this->config->set('URI.DisableResources', true);
+
+        $this->purifier = HTMLPurifier::getInstance($this->config);
+
+        $this->assertPurification('<img src="foo.jpg" />', '');
+    }
+
+    /**
+     * Asserts a purification. Good for integration testing.
+     * @param string $input
+     * @param string $expect
+     */
+    public function assertPurification($input, $expect = null)
+    {
+        if ($expect === null) {
+            $expect = $input;
+        }
+
+        $result = $this->purifier->purify($input, $this->config);
+        $this->assertEquals($expect, $result);
+    }
+}
